af0fc2a79220f54f4daf11dea8814c3fa9c52c17
Greg Morgan <gamm90@gmail.com>
2012-12-08 15:12:03 -0800

    Same as master

--- ConkyForWindows/MainWindow.xaml.cs
+++ ConkyForWindows/MainWindow.xaml.cs
@@ -15,114 +15,448 @@ using System.Diagnostics;
 using System.Threading;
 using System.ComponentModel;
 using System.Net;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Timers;
+using WUApiLib;
+using System.Management;
+using Microsoft.Win32;
+
 
 namespace WpfApplication1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xamlasdf
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+
         //public PerformanceCounter cpuCounter;
         public MainWindow()
         {
+             
             InitializeComponent();
-
+            
+            //this.WindowStartupLocation = WindowStartupLocation.Manual;
+        //    this.Left = -282;
+       //     this.Top = 35;
+            
             cpuUsage.Maximum = 100;
-
+            
             bw.WorkerSupportsCancellation = true;
             bw.WorkerReportsProgress = true;
             bw.DoWork +=
                 new DoWorkEventHandler(bw_DoWork);
             bw.ProgressChanged +=
                 new ProgressChangedEventHandler(bw_ProgressChanged);
-            bw.RunWorkerCompleted +=
-                new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+            
         }
 
         BackgroundWorker bw = new BackgroundWorker();
-
-        private void Async_Click(object sender, RoutedEventArgs e)
-        {
-            if (bw.IsBusy != true)
-            {
-                bw.RunWorkerAsync();
-            }
-        }
 
         private void cancel_Click(object sender, RoutedEventArgs e)
         {
             if (bw.WorkerSupportsCancellation == true)
             {
                 bw.CancelAsync();
+                this.Close();    
             }
         }
+
+        public string proc;
+        public double ramTotal;
+        public double ramUsed;
+        public double ramFree;
+        public double ramPercent;
+        public double driveSpace;
+        public string currentUsageReceived;
+        public string bytesSent;
+        public string totalSent;
+        public string totalReceived;
+        public string bytesRecieved;
         public float asdfa;
         private int number = 0;
+        public string updates;
+        public bool netavailable;
+        public bool netavailable2;
+        public string ipLocal;
+        public string ipExternal;
+        public bool test;
+        public string time;
+        public string cpuCount;
+
+        public NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
         public PerformanceCounter cpuCounter = new PerformanceCounter();
+         
+        //Does all the work on a background thread
+        
         private void bw_DoWork(object sender, DoWorkEventArgs e)
         {
+            
             BackgroundWorker worker = sender as BackgroundWorker;
-
+            
             cpuCounter = new PerformanceCounter();
+
             cpuCounter.CategoryName = "Processor";
             cpuCounter.CounterName = "% Processor Time";
             cpuCounter.InstanceName = "_Total";
             float[] avg = new float[10];
-            while (true)//for (int i = 1; (i <= 10); i++)
+
+            while (true)
             {
-                //HTTPGet req = new HTTPGet();
-                //req.Request("http://checkip.dyndns.org");
-                //string[] a = req.ResponseBody.Split(':');
-                //string a2 = a[1].Substring(1);
-                //string[] a3 = a2.Split('<');
-                //string a4 = a3[0];
-                //Console.WriteLine(a4);
                 if ((worker.CancellationPending == true))
                 {
                     e.Cancel = true;
                     break;
                 }
                 else
-                {
+                {   
                     worker.ReportProgress((number++));
                     for (int ii = 0; ii < 9; ii++)
                     {
                         avg[ii] = cpuCounter.NextValue();
                         asdfa = avg.Average();
                         string aasdf = asdfa.ToString();
+
                         Thread.Sleep(100);
                     }
+                    
+                    //Grabs Needed Info For Disk Space etc...
+                    DriveInfo[] drives
+                        = DriveInfo.GetDrives();
 
+                    DriveInfo drive = drives[0];
+                    driveSpace = drive.AvailableFreeSpace / 1073741824.004733;
+                 
+                    //Grabs all the needed info for network usage
 
+                    netavailable2 = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+                 
+                    if (netavailable2 == true)
+                    {
+                        NetworkInterface ni = interfaces[2];
+
+                        totalSent = (ni.GetIPv4Statistics().BytesSent / 1048576.0).ToString("f2") + " MB";
+                        totalReceived = (ni.GetIPv4Statistics().BytesReceived / 1048576.0).ToString("f2") + " MB";
+                    }
+                    else if (netavailable2 == false)
+                    {
+                        totalReceived = "Disconnected";
+                        totalSent = "Disconnected";
+                    }
+
+                      if (netavailable2 == true && number <= 1)
+                      {
+
+                          //Grabs External IP Address, and uses the HTTPGet Class
+                          HTTPGet req = new HTTPGet();
+                          req.Request("http://checkip.dyndns.org");
+                          string[] a = req.ResponseBody.Split(':');
+                          string a2 = a[1].Substring(1);
+                          string[] a3 = a2.Split('<');
+                          string a4 = a3[0];
+                          ipExternal = a4;
+
+                          //Grabs Local IP Address
+                          ipLocal = "";
+                          IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+                          int iter = 0;
+                          foreach (IPAddress i in localIPs)
+                          {
+                              string IP = i.ToString();
+                              bool isIP = IP.Contains(".");
+                              if (isIP == true)
+                                  ipLocal += IP + "\r\n";
+                              iter++;
+                          }
+                      }
+
+                    // Calculates RAM free, usage, total, and usage in percent
+                    if (number <= 1)
+                    {
+                        ramTotal = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / 1073741824.004733;
+                    } 
+                    ramFree = new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory / 1073741824.004733;
+                    ramUsed = ramTotal - ramFree;
+                    ramPercent =ramUsed  / ramTotal * 100;
+
+                    time = DateTime.Now.ToShortTimeString();
+
+                    /* Possible Future Implementation of System Hardware/Software Info like OS Version, CPU Type etc..
+                     * 
+                    RegistryKey Rkey = Registry.LocalMachine;
+                    Rkey = Rkey.OpenSubKey("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0");
+                    cpuCount = (string)Rkey.GetValue("ProcessorNameString") + "\n" + new Microsoft.VisualBasic.Devices.ComputerInfo().OSFullName;
+                     *
+                     */
+ 
+
+                    if ( netavailable2 == true && number == 3)
+                    {
+                        RSS = weather.CurrentConditions();
+                        WeatherImage = new Uri(weather.getImage());
+                    }/*
+                    else if (netavailable2 == true && number == 4)
+                    {
+                        UpdateSessionClass uSession = new UpdateSessionClass();
+                        IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+                        ISearchResult uResult = uSearcher.Search("IsInstalled=0");
+
+                        foreach (IUpdate update in uResult.Updates)
+                        {
+                            updateCount++;
+                        }
+                    }*/
+                    else if (netavailable2 == false)
+                    {
+                        updates = "Disconnected";
+                    }
                 }
+
             }
         }
+
+        //Update GUI here with info from the background thread
+       
         private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            //this.resultLabel.Text = (e.ProgressPercentage.ToString() + "%");
-            avgBox.Text = asdfa.ToString();
+            
+            //this.resultLabel.Text = (e.ProgressPercentage.ToString());
+            txtAvgBox.Text = asdfa.ToString("f2") + "%";
             cpuUsage.Value = asdfa;
 
+            // Prints to textbox network usage
+            txtTotalReceived.Text = totalReceived;
+            txtTotalSent.Text = totalSent;
+
+            if (netavailable == true)
+            {
+                txtNetOut.Text = bytesSent;
+                txtNetIn.Text = bytesRecieved;
+            }
+
+            txtPing.Text = pingtime;
+            txtLocal.Text = ipLocal;
+            txtExternal.Text = ipExternal;
+
+            //Converts Variables to a String so you can then format properly
+            Convert.ToString(ramTotal);
+            Convert.ToString(ramUsed);
+            Convert.ToString(ramFree);
+            Convert.ToString(ramPercent);
+            Convert.ToString(driveSpace);
+
+            //Prints free and used RAM to textboxes
+            txtRam.Text = ramFree.ToString("f2") + " GB";
+            txtRamPercent.Text = ramPercent.ToString("f2") + " %";
+            //txtRamUsed.Text = ramTotal.ToString("f2") + " GB";
+
+            //Prints Disk Drive Free Space
+            txtDriveSpace.Text = driveSpace.ToString("f2") + " GB";
+
+            //Shows the cooridinates of screen window
+            Point scrPos = this.PointToScreen(new Point(0, 0));
+            //txtPoint.Text = Convert.ToString(scrPos);
+
+            //Prints Available Updates to screen.
+            txtUpdates.Text = Convert.ToString(updateCount);
+
+            txtTime.Text = time;
+            txtWeather.Text = RSS;
+            imgCloudy.Source = new BitmapImage(WeatherImage);
+
         }
-        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+
+
+        public int updateCount = 0;
+        public string external;
+        private WeatherRSS.Weather weather = new WeatherRSS.Weather();
+        Uri WeatherImage = new Uri("http://s3.amazonaws.com/gt-production-icons/icons/icon/62225.jpg?1348580377");
+    
+        private string RSS;
+
+        public void pingEvent(object sender, ElapsedEventArgs e)
         {
-            if ((e.Cancelled == true))
-            {
-                //this.resultLabel.Text = "Canceled!";
-            }
+            netavailable = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+            
+                if (netavailable == false)
+                {
+                    pingtime = "Disconnected";
+                }
+                else if (netavailable == true)
+                {
+                    //tests ping time
+                    pingtime = Convert.ToString(GetPingMS("www.easytel.com"))+ " ms";
+                }        
+        }
 
-            else if (!(e.Error == null))
-            {
-                //this.resultLabel.Text = ("Error: " + e.Error.Message);
-            }
+        int GetPingMS(string hostNameOrAddress)
+        {
+            System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+            return Convert.ToInt32(ping.Send(hostNameOrAddress).RoundtripTime);
+        }
 
+        public System.Timers.Timer cTimer = new System.Timers.Timer();
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            cancel.Opacity = 0;
+          
+            bw.RunWorkerAsync();
+
+            RSS = "Collecting Candy...";
+
+            // Makes all textboxes readonly
+            txtTotalReceived.IsReadOnly = true;
+            txtTotalSent.IsReadOnly = true;
+            txtNetOut.IsReadOnly = true;
+            txtNetIn.IsReadOnly = true;
+            txtPing.IsReadOnly = true;
+            txtRam.IsReadOnly = true;
+            txtRamPercent.IsReadOnly = true;
+            //txtRamUsed.IsReadOnly = true;
+            txtDriveSpace.IsReadOnly = true;
+            //txtPoint.IsReadOnly = true;
+            txtUpdates.IsReadOnly = true;
+            txtLocal.IsReadOnly = true;
+            txtExternal.IsReadOnly = true;
+            txtWeather.IsReadOnly = true; 
+
+            // Make timer for network usage.
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            // Set the Interval to 1 seconds.
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
+
+            // Make timer for ping and IP Address time.
+            System.Timers.Timer bTimer = new System.Timers.Timer();
+            bTimer.Elapsed += new ElapsedEventHandler(pingEvent);
+
+            // Set the Interval to 5 seconds.
+            bTimer.Interval = 5000;
+            bTimer.Enabled = true;
+
+            // Make timer for System Update check, and weather.
+            // System.Timers.Timer cTimer = new System.Timers.Timer();
+            cTimer.Elapsed += new ElapsedEventHandler(fifteenMinutes);
+
+            // Set the Interval to 15 minutes.
+            cTimer.Interval = 1000;//900000;
+            cTimer.Enabled = true;
+        }
+
+     
+        private void fifteenMinutes(object sender, ElapsedEventArgs e)
+        {
+            cTimer.Interval = 900000;
+            //updateCount = 0;
+            UpdateSessionClass uSession = new UpdateSessionClass();
+            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+            ISearchResult uResult = uSearcher.Search("IsInstalled=0 and Type='Software'");
+            updateCount = uResult.Updates.Count;
+
+            if (netavailable == true)
+            {
+                RSS = weather.CurrentConditions();
+                WeatherImage = new Uri(weather.getImage());
+
+                //Grabs External IP Address, and uses the HTTPGet Class
+                HTTPGet req = new HTTPGet();
+                req.Request("http://checkip.dyndns.org");
+                string[] a = req.ResponseBody.Split(':');
+                string a2 = a[1].Substring(1);
+                string[] a3 = a2.Split('<');
+                string a4 = a3[0];
+                ipExternal = a4;
+
+                //Grabs Local IP Address
+                ipLocal = "";
+                IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+                int iter = 0;
+                foreach (IPAddress i in localIPs)
+                {
+                    string IP = i.ToString();
+                    bool isIP = IP.Contains(".");
+                    if (isIP == true)
+                        ipLocal += IP + "\r\n";
+                    iter++;
+                }
+            }
+            
+        }
+
+        private double upLoadOld = 0.0;
+        private double upLoadNew = 0.0;
+        private double upLoadTotal = 0.0;
+        private double downLoadOld = 0.0;
+        private double downLoadNew = 0.0;
+        private double downLoadTotal = 0.0;
+        private string pingtime;
+        
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            //Grabs all the needed info for network usage
+            NetworkInterface ni = interfaces[2];
+            
+            upLoadNew = (ni.GetIPv4Statistics().BytesSent / 131072.0);
+            upLoadTotal = upLoadNew - upLoadOld;
+
+            //changes text between Mbps and Kbps
+            if (netavailable == true)
+            {
+                if (upLoadTotal > 1)
+                {
+                    bytesSent = (upLoadTotal).ToString("f2") + " Mbps";
+                    upLoadOld = upLoadNew;
+                }
+                else
+                {
+                    bytesSent = (upLoadTotal * 1024).ToString("f2") + " Kbps";
+                    upLoadOld = upLoadNew;
+                }
+
+                downLoadNew = (ni.GetIPv4Statistics().BytesReceived / 131072.0);
+                downLoadTotal = downLoadNew - downLoadOld;
+                if (downLoadTotal > 1)
+                {
+                    bytesRecieved = (downLoadTotal).ToString("f2") + " Mbps";
+                    downLoadOld = downLoadNew;
+                }
+                else
+                {
+                    bytesRecieved = (downLoadTotal * 1024).ToString("f2") + " Kbps";
+                    downLoadOld = downLoadNew;
+                }
+            }
+            
+
+        }
+
+        private void locationWindow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (locationWindow.WindowStyle == System.Windows.WindowStyle.None)
+            {
+
+                locationWindow.WindowStyle = System.Windows.WindowStyle.SingleBorderWindow;
+                cancel.Opacity = 0;
+                this.Left -= 3;
+                this.Top -= 26;
+                this.Height += 20;
+            }
             else
             {
-                //this.resultLabel.Text = "Done!";
-                //avgBox.Text = asdfa.ToString();
+                locationWindow.WindowStyle = System.Windows.WindowStyle.None;
+
+                cancel.Opacity = 100;
+                this.Left += 3;
+                this.Top += 26;
+                this.Height -= 20;
             }
-        }
-        
+        }        
     }
-}
+ }
+
+
+ 
---
+++ ConkyForWindows/Weather.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Net;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+using System.Timers;
+using WUApiLib;
+using System.Management;
+using Microsoft.Win32;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace WeatherRSS
+{
+    class Weather
+    {
+        //public bool cloudy;
+        public string CurrentConditions()
+        {
+            
+            string weather = "";
+
+            // http://weather.yahooapis.com/forecastrss?w=2464601
+
+            // conditions
+            // Create a new XmlDocument  
+            XmlDocument condition = new XmlDocument();
+
+            // Load data  
+            condition.Load("http://weather.yahooapis.com/forecastrss?w=2464601");
+
+            // Set up namespace manager for XPath  
+            XmlNamespaceManager NameSpaceMgrCondition = new XmlNamespaceManager(condition.NameTable);
+            NameSpaceMgrCondition.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            // Get forecast with XPath  
+            XmlNodeList nodes = condition.SelectNodes("/rss/channel/item/yweather:condition", NameSpaceMgrCondition);
+
+            // To get forcasted high
+            string temps = forcastTemps();
+
+            foreach (XmlNode node in nodes)
+            {
+                
+                weather = ("Currently: " +
+                                    node.Attributes["text"].InnerText + "\n" +
+                                    "Now: " +
+                                    node.Attributes["temp"].InnerText + "\n" +
+                                    "High: " +
+                                    temps + "\n" + 
+                                    "Last Updated: " +
+                                    node.Attributes["date"].InnerText);
+
+            }
+
+           
+            return weather;
+        }
+
+        private string forcastTemps()
+        {
+            string weather = "";
+
+            // forcast
+            // Create a new XmlDocument  
+            XmlDocument docc = new XmlDocument();
+
+            // Load data  
+            docc.Load("http://xml.weather.yahoo.com/forecastrss?w=2464601");
+
+            // Set up namespace manager for XPath  
+            XmlNamespaceManager forcastNameSpaceMgr = new XmlNamespaceManager(docc.NameTable);
+            forcastNameSpaceMgr.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            // Get forecast with XPath  
+            XmlNodeList nodess = docc.SelectNodes("/rss/channel/item/yweather:forecast", forcastNameSpaceMgr);
+            int i = 0;
+            foreach (XmlNode node in nodess)
+            {
+                if (i == 0)
+                    weather = node.Attributes["high"].InnerText + "\nLow: " + node.Attributes["low"].InnerText;
+                i++;
+            }
+            return weather;
+        }
+
+        public string Forcast()
+        {
+            string weather = "";
+
+            // forcast
+            // Create a new XmlDocument  
+            XmlDocument docc = new XmlDocument();
+
+            // Load data  
+            docc.Load("http://xml.weather.yahoo.com/forecastrss?w=2464601");
+
+            // Set up namespace manager for XPath  
+            XmlNamespaceManager forcastNameSpaceMgr = new XmlNamespaceManager(docc.NameTable);
+            forcastNameSpaceMgr.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            // Get forecast with XPath  
+            XmlNodeList nodess = docc.SelectNodes("/rss/channel/item/yweather:forecast", forcastNameSpaceMgr);
+
+            // You can also get elements based on their tag name and namespace,  
+            // though this isn't recommended  
+            //XmlNodeList nodes = doc.GetElementsByTagName("forecast",   
+            //                          "http://xml.weather.yahoo.com/ns/rss/1.0");  
+            weather += "\nForcast";
+            foreach (XmlNode node in nodess)
+            {
+                weather += ("\n" +
+                                    node.Attributes["day"].InnerText + "\n" +
+                                    "The Condition will be " +
+                                    node.Attributes["text"].InnerText + "\n" +
+                                    "High: " +
+                                    node.Attributes["high"].InnerText + "\n" +
+                                    "Low: " +
+                                    node.Attributes["low"].InnerText);
+            }
+            return weather;
+        }
+
+        public string getImage()
+        {
+            // forcast
+            // Create a new XmlDocument  
+            XmlDocument doc = new XmlDocument();
+
+            // Load data  
+            doc.Load("http://xml.weather.yahoo.com/forecastrss?w=2464601");
+
+            // Set up namespace manager for XPath  
+            XmlNamespaceManager ImageNameSpaceMgr = new XmlNamespaceManager(doc.NameTable);
+            ImageNameSpaceMgr.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            // Get forecast with XPath  
+            XmlNodeList nodess = doc.SelectNodes("/rss/channel/item/description", ImageNameSpaceMgr);
+
+
+            string xml = nodess[0].InnerXml;
+            string desiredValue = Regex.Replace(xml
+                                           .Replace("<br />", "\n")
+                                           .Trim(),
+                    @"\<br />", "\n");
+            desiredValue = Regex.Replace(desiredValue
+                                           .Replace("<br />", "\n")
+                                           .Trim(),
+                    @"\<BR />", "\n");
+
+            desiredValue = desiredValue.Remove(57);
+            desiredValue = desiredValue.Remove(0, 20);
+
+            return desiredValue;
+        }
+    }
+}
